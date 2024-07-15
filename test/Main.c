7d99187362130cfdd0acb2ec611887c0b0c23055
omni-drft <piotrwalczak.pv@gmail.com>
2024-07-15 14:47:59 +0200

    Succesfully creating window with the library

--- test/Main.c
+++ test/Main.c
@@ -1,8 +1,22 @@
+#include <stdbool.h>
+
 #include "peBase.h"
+
 
 int main()
 {
     peState state;
     peInit(&state, "Testowe Okno", 400, 400);
 
+    while (!state.quit)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                state.quit = true;
+            break;
+
+        }
+    }
 }
